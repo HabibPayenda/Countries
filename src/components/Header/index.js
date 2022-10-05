da84e1270005235aa925849abb298646abff6532
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-10-05 15:53:15 +0430

    Adds the south asia map

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-unneeded-ternary */
 /* eslint-disable react/prop-types */
 import React from 'react';
+import map from '../../img/map.png';
 
 function Header({ population, title = 'Southern Asia', flag }) {
   return (
     <div className="header">
       <div className="headerLeft">
-        <img className="headerImg" src={flag} alt={title} />
+        <img className="headerImg" src={flag ? flag : map} alt={title} />
       </div>
       <div className="headerRight">
         <p className="headerRightTitle">
